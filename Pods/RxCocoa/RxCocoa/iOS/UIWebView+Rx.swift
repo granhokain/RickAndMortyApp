9eeb55e2489ddbd95741187e01f6117e3c6441b2
rafaelmartins <rafaelmartins@ioasys.com.br>
2021-03-05 08:44:05 -0300

    Fix pod issues.

--- Pods/RxCocoa/RxCocoa/iOS/UIWebView+Rx.swift
+++ Pods/RxCocoa/RxCocoa/iOS/UIWebView+Rx.swift
@@ -10,37 +10,38 @@
 
     import UIKit
     import RxSwift
+    import WebKit
 
-    extension Reactive where Base: UIWebView {
+    extension Reactive where Base: WKWebView {
 
-        /// Reactive wrapper for `delegate`.
-        /// For more information take a look at `DelegateProxyType` protocol documentation.
-        public var delegate: DelegateProxy<UIWebView, UIWebViewDelegate> {
-            return RxWebViewDelegateProxy.proxy(for: base)
-        }
-
-        /// Reactive wrapper for `delegate` message.
-        public var didStartLoad: Observable<Void> {
-            return delegate
-                .methodInvoked(#selector(UIWebViewDelegate.webViewDidStartLoad(_:)))
-                .map { _ in }
-        }
-
-        /// Reactive wrapper for `delegate` message.
-        public var didFinishLoad: Observable<Void> {
-            return delegate
-                .methodInvoked(#selector(UIWebViewDelegate.webViewDidFinishLoad(_:)))
-                .map { _ in }
-        }
-        
-        /// Reactive wrapper for `delegate` message.
-        public var didFailLoad: Observable<Error> {
-            return delegate
-                .methodInvoked(#selector(UIWebViewDelegate.webView(_:didFailLoadWithError:)))
-                .map { a in
-                    return try castOrThrow(Error.self, a[1])
-                }
-        }
+//        /// Reactive wrapper for `delegate`.
+//        /// For more information take a look at `DelegateProxyType` protocol documentation.
+//        public var delegate: DelegateProxy<UIWebView, UIWebViewDelegate> {
+//            return RxWebViewDelegateProxy.proxy(for: base)
+//        }
+//
+//        /// Reactive wrapper for `delegate` message.
+//        public var didStartLoad: Observable<Void> {
+//            return delegate
+//                .methodInvoked(#selector(UIWebViewDelegate.webViewDidStartLoad(_:)))
+//                .map { _ in }
+//        }
+//
+//        /// Reactive wrapper for `delegate` message.
+//        public var didFinishLoad: Observable<Void> {
+//            return delegate
+//                .methodInvoked(#selector(UIWebViewDelegate.webViewDidFinishLoad(_:)))
+//                .map { _ in }
+//        }
+//
+//        /// Reactive wrapper for `delegate` message.
+//        public var didFailLoad: Observable<Error> {
+//            return delegate
+//                .methodInvoked(#selector(UIWebViewDelegate.webView(_:didFailLoadWithError:)))
+//                .map { a in
+//                    return try castOrThrow(Error.self, a[1])
+//                }
+//        }
     }
 
 #endif
